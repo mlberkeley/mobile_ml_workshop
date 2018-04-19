95cff362fd55d60c27872c46ed4799dbaa4bb24f
Gokul Swamy <gokul.swamy@berkeley.edu>
2018-04-18 21:23:02 -0700

    release server code

--- server.py
+++ server.py
@@ -11,10 +11,25 @@ class Handler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        # TODO
+        self._set_headers()
+        with open("./user_ids.txt") as file:
+            id_str = ""
+            for line in file:
+                id_str += (line + "<br>")
+            self.wfile.write(str.encode("<html><body><h1>Current Users:</h1>" + id_str + "</body></html>"))
         
     def do_POST(self):
-        # TODO
+        self._set_headers()
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = str(self.rfile.read(content_len))
+        cleaned = post_body.replace('\\n', '').replace('\\r', '').replace('\\', '').replace(' ', '')[2:-1]
+        result = json.loads(cleaned)
+        label = result['label']
+        print(label)
+        with open('./images/' + label + "_" + result['id'] + "_" + str(np.random.randint(1000000)) + ".png", "wb") as fh:
+            fh.write(base64.decodebytes(str.encode(result['img'])))
+        with open("./user_ids.txt", 'a') as file:
+            file.write(result['id'] + '\n')  
 
 
 def run(server_class=HTTPServer, handler_class=Handler, port=8000):
@@ -24,4 +39,4 @@ def run(server_class=HTTPServer, handler_class=Handler, port=8000):
     server.serve_forever()
 
 if __name__ == '__main__':
-	run()
+    run()
